d27d23c8a2d8c9889c20c51d70bea23357f16dea
Ananya Jain <ananyaj1515@gmail.com>
2025-12-14 11:12:55 +0530

    implement hoc for managing window states

---
+++ src/windows/Terminal.jsx
@@ -0,0 +1,54 @@
+import { WindowControls } from "#components";
+import WindowWrapper from "#components/hoc/WindowWrapper"
+import { techStack } from "#constants";
+import { CheckIcon, FlagIcon } from "lucide-react";
+
+const Terminal = () => {
+    return (
+        <>
+            <div id="window-header">
+                <WindowControls target="terminal"/>
+                <h2>Tech Stack</h2>
+            </div>
+            <div className="techstack">
+                <p>
+                    <span className="font-bold">@ananya %</span>
+                    show tech stack
+                </p>
+                <div className="label">
+                    <p className="w-32">
+                        Category
+                    </p>
+                    <p>Technologies</p>
+                </div>
+                <ul className="content">
+                    {techStack.map(({category, items}) => (
+                        <li className="flex items-center" key="Category">
+                            <CheckIcon className="check" size={20}/>
+                            <h3>{category}</h3>
+                            <ul>
+                                {items.map((item, i) => (
+                                    <li key={i}>{item}{i < items.length - 1 ? ',': ''}</li>
+                                ))}
+                            </ul>
+                           
+                        </li>
+                    ))}
+                </ul>
+                 <div className="footnote">
+                    <p>
+                        <CheckIcon size={20} /> 5/5 stacks loaded successfully (100%)
+                    </p>
+                    <p className="text-black">
+                        <FlagIcon size={15} fill="black" />
+                        Render time: 6 ms
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const TerminalWindow = WindowWrapper(Terminal, 'terminal')
+
+export default TerminalWindow;
