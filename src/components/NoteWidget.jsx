6e40c247138842723e35d263b89c420e24a6b451
Ananya Jain <ananyaj1515@gmail.com>
2026-06-27 17:08:42 +0800

    Update widget image

--- src/components/NoteWidget.jsx
+++ src/components/NoteWidget.jsx
@@ -3,10 +3,16 @@ import WidgetWrapper from "./hoc/WidgetWrapper";
 const NoteWidget = () => {
   return (
     <WidgetWrapper>
-        <div className=" bg-amber-200 w-full h-full overflow-hidden p-5 border border-5 border-amber-300">
-            <h1 className="font-mono">
-                Computer Science Student @ NUS
-            </h1>
+        <div className=" bg-amber-200 w-full h-full overflow-hidden p-5 border-4 border-amber-300">
+            <div className="flex-row gap-y-5">
+                <div>
+                    <h1 className="font-roboto text-2xl">About Me</h1>
+                </div>
+                <div className="font-mono">
+                   <p>Computer Science</p>
+                   <p>NUS'28</p>
+                </div>
+                </div>
         </div>
     </WidgetWrapper>
   );
