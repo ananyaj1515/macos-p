1eeb7c18f8a3be08f2cd252444e65c329fca1dec
Ananya Jain <ananyaj1515@gmail.com>
2026-06-27 20:49:09 +0800

    Add information to note widget

--- src/components/NoteWidget.jsx
+++ src/components/NoteWidget.jsx
@@ -1,18 +1,41 @@
+import { GraduationCap, Laptop, BookOpenCheck} from 'lucide-react';
 import WidgetWrapper from "./hoc/WidgetWrapper";
 
 const NoteWidget = () => {
   return (
     <WidgetWrapper>
-        <div className=" bg-amber-200 w-full h-full overflow-hidden p-5 border-4 border-amber-300">
-            <div className="flex-row gap-y-5">
-                <div>
-                    <h1 className="font-roboto text-2xl">About Me</h1>
+        <div className=" bg-[#f3ec9aa6] w-full h-full overflow-hidden p-5 border-4 border-[#b5952a4e]">
+          <div className="flex flex-col justify-between gap-5">
+              
+                   
+            <div>
+              <h1 className="font-roboto text-2xl">About Me</h1>
+              <h1>---------</h1>
+            </div>
+            
+            <div className='flex flex-row align-middle'>
+              <GraduationCap size={30}/>
+              <div className="font-mono pl-5 flex flex-col">
+                <p>Computer Science & Entrepreneurship @ NUS </p>
+                <p>Class of 2028</p>
+              </div>
+            </div>
+
+                <div className='flex flex-row align-middle'>
+                  <Laptop size={50}/>
+                  <div className="font-mono pl-5 flex flex-col">
+                    <p>Specialising in Full Stack Development, AI & Parallel Computing </p>
+                  </div>
                 </div>
-                <div className="font-mono">
-                   <p>Computer Science</p>
-                   <p>NUS'28</p>
+
+                <div className='flex flex-row align-middle'>
+                  <BookOpenCheck size={30}/>
+                  <div className="font-mono pl-5 flex flex-col">
+                    <p>Currently learning: .NET, C# & Langchain </p>
+                  </div>
                 </div>
-                </div>
+                
+          </div>
         </div>
     </WidgetWrapper>
   );
